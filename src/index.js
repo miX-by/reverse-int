3f5ba27a6b114f1163f4f66f5d8ec236db23d088
miX-by <mike.sormanov@gmail.com>
2021-03-15 18:44:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const string = String(Math.abs(n));
+    const arr = string.split("");
+    const revarr = arr.reverse();
+    const stringTrue = revarr.join("");
+    const result = Number(stringTrue);
+    return result;
+};
